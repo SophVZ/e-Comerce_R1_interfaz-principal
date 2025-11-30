7531d3be7b0d2833a70a2511e3b8fde9ae282968
Sofi VZ <sv.zuccar@gmail.com>
2025-11-30 18:37:13 -0300

    responsividad menu

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,9 +1,29 @@
 
 $(document).ready(function(){
-    $('scrollToTop').on('click',function(event){
+    // filtro sacado de google ia
+    $('.nav-filtro-btn').on('click', function(event){
+        event.preventDefault();
+        //categoria del enlace
+        var filtro = $(this).data('filter')
+        //oculta tarjetas
+        $('.producto-item').addClass('d-none');
+        //muestra tarjetas filtradas
+        if (filtro === 'todos') {
+            $('.producto-item').removeClass('d-none')
+        } else {
+            // selecciona elementos con la data.categoria
+            $('.producto-item[data-categoria*="' + filtro + '"]').removeClass('d-none')
+        }
+        //resalta enlace activo
+        $('.nav-filtro-btn').removeClass('active');
+        $(this).addClass('active')
+    });
+
+    $('#scrollToTop').on('click',function(event){
         event.preventDefault();
         $('html, body').animate({
             scrollTop: 0
         }, 800);
     });
+
 });
