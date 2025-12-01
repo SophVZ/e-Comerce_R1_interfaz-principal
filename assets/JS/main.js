25a992b85a4c444792b57a34ba1f4234305137aa
Sofi VZ <sv.zuccar@gmail.com>
2025-11-30 22:40:09 -0300

    agregar al carrito

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,42 @@
 
 $(document).ready(function(){
+
+    // agregar al carrito
+    var cart = JSON.parse(localStorage.getItem('shoppingCart')) || {};
+    function updateCartCounter() {
+        var totalItems = 0;
+        for (var id in cart) {
+            totalItems += cart[id].quantity;
+        }
+        $('#cart-counter').text(totalItems)
+    }
+
+    function saveCart() {
+        localStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+
+    $('.add-to-cart-btn').on('click', function(e) {
+        e.preventDefault();
+        var productId = $(this).data('product-id');
+        var productPrice = $(this).data('product-price');
+        var productName = $(this).siblings('h3.h5').text();
+
+        if (cart[productId]) {
+            cart[productId].quantity++;
+        } else {
+            cart[productId] = {
+                name: productName,
+                price: productPrice,
+                quantity: 1
+            };
+        }
+        saveCart();
+        updateCartCounter();
+        alert(productName + "Añadido al carrito");
+    });
+
+    updateCartCounter();
+
     // filtro sacado de google ia
     $('.nav-filtro-btn').on('click', function(event){
         event.preventDefault();
